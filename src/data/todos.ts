d0f972251badd47fa58c3059291041df1635d657
Igorth <igordiasth@gmail.com>
2024-03-06 06:43:51 -0500

    feat: add TodoItem component and style

--- src/data/todos.ts
+++ src/data/todos.ts
@@ -3,17 +3,17 @@ import { Todo } from '../types/todo';
 export const dummyData: Todo[] = [
   {
     id: 1,
-    title: 'Buy groceries',
-    completed: false,
+    title: 'Buy groceries 1',
+    completed: true,
   },
   {
     id: 2,
-    title: 'Buy groceries',
+    title: 'Buy groceries 2',
     completed: false,
   },
   {
     id: 3,
-    title: 'Buy groceries',
+    title: 'Buy groceries 3',
     completed: false,
   },
 ];
