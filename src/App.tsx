ccaff9b3e57296800bef4dbe9bc9c1cb433d3ac3
Igorth <igordiasth@gmail.com>
2024-03-06 20:52:28 -0500

    feat: add callback props TodoItem

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import TodoItem from './components/TodoItem';
 import { dummyData } from './data/todos';
 
 function App() {
+  function setTodoCompleted(id: number, completed: boolean) {
+    alert(
+      `Todo with ${id} is now ${completed ? 'completed' : 'not completed'}`
+    );
+  }
+
   return (
     <main className="py-10 h-screen space-y-10">
       <h1 className="font-bold text-3xl text-center">Best TODOS</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-3">
           {dummyData.map(todo => (
-            <TodoItem todo={todo} />
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onCompletedChange={setTodoCompleted}
+            />
           ))}
         </div>
       </div>
